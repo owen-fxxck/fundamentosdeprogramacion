04c8593a889a369f0fad5822dc4357eb682c049e
owen <l24121325@morelia.tecnm.mx>
2024-09-12 22:01:41 -0600

    tarea4/unidad2

---
+++ Tarea4_unidad2/src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Primera Operacion
+        double dividendo1 = ((12+8)*(5-3)-(6/2));
+        double divisor1 = ((3*4)+(8/2));
+        double resultado1 = dividendo1/divisor1;
+        System.out.println(resultado1);
+
+        // Segunda Operacion
+        double dividendo2 = (((9*5)-(15+3)+(8*2)-(12/4))*(7+2)-((6*3)/2))/(2+((3*2)/(4-2)))+(5*((8/4)+1))-(6/(2+1));
+        double divisor2 = ((((4+2)*(7-1))/(3+1))+((3*2)-(5+1))*(8/2))/(3+((6/2)+(7/2)))+(2*(3+(8/4)));
+        double resultado2 = dividendo2/divisor2;
+        System.out.println(resultado2);
+
+        // Tercera Operacion
+        double divdendo3 = ((6 + 4) * 3) - (18 / 2);
+        double divisor3 = (8 - 5) + (3 * 2);
+        double resultado3 = divdendo3/divisor3;
+        System.out.println(resultado3);
+    }
+}
